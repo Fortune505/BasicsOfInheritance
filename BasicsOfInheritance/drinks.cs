df8273a3ad1602bcf172d5aedc39a265ef738244
Fortune505 <forturn397@gmail.com>
2026-03-29 14:48:17 +0800

    Исправление и раработка файла drinks.cs

--- BasicsOfInheritance/drinks.cs
+++ BasicsOfInheritance/drinks.cs
@@ -8,22 +8,24 @@ namespace BasicsOfInheritance
 {
     public class Drinks
     {
+        public double Volume = 0.0;
         public virtual String GetInfo()
         {
-            return "Я напиток";
+            return String.Format("Объем: {0} л.", this.Volume);
         }
 
     }
     public class Juice : Drinks
     {
-        public double Volume = 0.0;
         public string FruitsUsed = "";
         public bool PresenceOfPulp = false;
 
         public override String GetInfo()
         {
-            var str = "Я сок";
-            str += String.Format("\nОбъем {0}", this.Volume);
+            var str = "Я сок\n";
+            str += base.GetInfo();
+            str += String.Format("\nФрукты: {0}", this.FruitsUsed);
+            str += String.Format("\nНаличие мякоти: {0}", this.PresenceOfPulp ? "Да" : "Нет");
             return str;
         }
     }
@@ -31,14 +33,16 @@ namespace BasicsOfInheritance
     public enum SodaView {sweet, mineralWater}
     public class Soda : Drinks
     {
-        public double Volume = 0.0;
+        
         public int NumberOfBubbles = 0;
         public SodaView view = SodaView.sweet;
 
         public override String GetInfo()
         {
-            var str = "Я газировка";
-            str += String.Format("\nОбъем {0}", this.Volume);
+            var str = "Я газировка\n";
+            str += base.GetInfo();
+            str += String.Format("\nКоличество пузырьков: {0}", this.NumberOfBubbles);
+            str += String.Format("\nВид: {0}", this.view == SodaView.sweet ? "Сладкая" : "Минеральная");
             return str;
         }
     }
@@ -46,14 +50,16 @@ namespace BasicsOfInheritance
     public enum AlcoholType {fermented, distillate, rectifiedSpirit}
     public class Alcohol : Drinks
     {
-        public double Volume = 0.0;
+        
         public int Fortress = 0;
         public AlcoholType type = AlcoholType.fermented;
 
         public override String GetInfo()
         {
-            var str = "Я алкаголь";
-            str += String.Format("\nОбъем {0}", this.Volume);
+            var str = "Я алкаголь\n";
+            str += base.GetInfo();
+            str += String.Format("\nКрепость: {0}%", this.Fortress);
+            str += String.Format("\nТип: {0}", this.type.ToString());
             return str;
         }
     }
